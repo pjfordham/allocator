7ca35eb99b92a102f9c7996e5079883862febac6
Peter Fordham <peter.fordham@gmail.com>
2020-05-22 19:42:42 -0700

    Switch to libfmt for messages, improve destructor handling, add dump state command, simplify some logic with sizeof.

--- allocator.cc
+++ allocator.cc
@@ -1,14 +1,14 @@
 #include "allocator.hh"
-#include <iostream>
+#include <fmt/format.h>
 
 struct A {
    int x;
    int y;
    A(): x(0), y(0) {
-      std::cout << "A()  " << (uintptr_t)this<< "\n";
+      fmt::print(" A(): {:x}\n", (uintptr_t)this );
    }
    ~A() {
-      std::cout << "~A() " << (uintptr_t)this<< "\n";
+      fmt::print("~A(): {:x}\n", (uintptr_t)this );
    }
 };
 
@@ -16,11 +16,16 @@ struct B {
    int x;
    int y;
    B(): x(0), y(0) {
-      std::cout << "B()  " << (uintptr_t)this<< "\n";
+      fmt::print(" B(): {:x}\n", (uintptr_t)this );
    }
    ~B() {
-      std::cout << "~B() " << (uintptr_t)this<< "\n";
+      fmt::print("~B(): {:x}\n", (uintptr_t)this );
    }
+};
+
+struct C {
+   int x;
+   int y;
 };
 
 int main(int argc, const char** argv) {
@@ -29,11 +34,17 @@ int main(int argc, const char** argv) {
    B* b = z_allocate_and_construct<B>(5, 1,  reinterpret_cast<uintptr_t>(&b));
    A* c = z_allocate_and_construct<A>(2, 2,  reinterpret_cast<uintptr_t>(&c));
    B* d = z_allocate_and_construct<B>(4, 3,  reinterpret_cast<uintptr_t>(&d));
-   
+
+   C* e = z_allocate_and_construct<C>(4, 3,  reinterpret_cast<uintptr_t>(&d));
+
+   z_dump();
    z_free_all_with_tag( 1 );
    z_free( c );
+   z_dump();
    z_free( d );
-   
+   z_free( e );
+   z_dump();
+
+
    return 0;
 }
-
--- allocator.hh
+++ allocator.hh
@@ -1,52 +1,87 @@
 #include <map>
 #include <functional>
 #include <cstdint>
-#include <iostream>
+#include <type_traits>
+#include <fmt/format.h>
 
 struct alloc_info_t {
-   std::function<void(void*)> destructor;
+   bool trivially_destructible;
    std::size_t count;
    std::uintptr_t tagA;
    std::uintptr_t tagB;
    std::size_t sizeoftype;
+   std::function<void(void*)> destructor;
+   // could have some string based on __func__ to see typename
+   // or https://en.cppreference.com/w/cpp/types/type_index
 };
 
 std::map<uintptr_t, alloc_info_t> memory;
 
 template<typename Type>
 Type* z_allocate_and_construct( std::size_t count, std::uintptr_t tagA, std::uintptr_t tagB) {
-   alloc_info_t info;
-   info.sizeoftype = sizeof(Type);
-   auto ptr = malloc( info.sizeoftype * count );
+   alloc_info_t info {
+      std::is_trivially_destructible<Type>::value,
+         count,
+         tagA,
+         tagB,
+         sizeof(Type) };
 
-   // manually call constructors
-   for (int i = 0; i< count ;i++ ) {
-      new ((char*)ptr + info.sizeoftype * i) Type;
+   auto ptr = static_cast<Type*>( malloc( info.sizeoftype * count ) );
+
+   // This is probably redundant.
+   if (!std::is_trivially_constructible<Type>::value) {
+      // manually call constructors
+      for (int i = 0; i< count ;i++ ) {
+         new (ptr + i) Type;
+      }
+   } else {
+      fmt::print( " Trivial(): {:x}\n", (std::uintptr_t)ptr );
    }
-   info.destructor = [] ( void *ptr ) { reinterpret_cast<Type*>(ptr)->~Type(); } ;
-   info.count = count;
-   info.tagA = tagA;
-   info.tagB = tagB;
+
+   if ( !info.trivially_destructible) {
+      info.destructor = [=, count = info.count] ( void *mem) {
+         auto ptr = static_cast<Type*>(mem);
+         for (int i = 0; i< count ;i++ ) {
+            (ptr + i)->~Type();
+         }
+      } ;
+   }
    memory[ (uintptr_t)ptr ] = info;
-   return static_cast<Type*>(ptr);
+   return ptr;
 }
 
 template<typename Type>
 void z_free( Type *mem ) {
    alloc_info_t info = memory[ (uintptr_t)mem ];
 
-   for (int i = 0; i< info.count ;i++ ) {
-      info.destructor( (char*)mem + info.sizeoftype * i );
+   if (!info.trivially_destructible) {
+      info.destructor( (void*)mem);
+   } else {
+      fmt::print( "~Trivial(): {:x}\n", (std::uintptr_t)mem );
    }
    free(mem);
+   memory.erase( (uintptr_t)mem );
 }
 
-void z_free_all_with_tag(  std::uintptr_t tagA ){
+static void z_free_all_with_tag(  std::uintptr_t tagA ){
    for (auto element : memory) {
       auto ptr = element.first;
       alloc_info_t &info = element.second;
       if (info.tagA == tagA) {
          z_free( (void*)ptr);
       }
+   }
+}
+
+static void z_dump( ){
+   fmt::print( "{:>16} {:>16} {:>16} {:>16} {:>16} {:>16} \n",
+               "Pointer", "No Destructor", "Count",
+               "tagA", "tagB","size of type");
+   for (auto element : memory) {
+      auto ptr = element.first;
+      alloc_info_t &info = element.second;
+      fmt::print( "{:>16x} {:>16} {:>16} {:>16} {:>16x} {:>16} \n",
+                  ptr, info.trivially_destructible, info.count,
+                  info.tagA, info.tagB, info.sizeoftype);
    }
 }
